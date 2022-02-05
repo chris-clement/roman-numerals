c211bbe13678a146903b5fce76460b80c983ad53
Chris Clement <chrisclement@Chriss-Air.connect>
2022-02-05 12:29:36 +0000

    Grouped tests under umbrella of single letters

--- tests/romanNumeralsConverter.test.js
+++ tests/romanNumeralsConverter.test.js
@@ -13,26 +13,28 @@ describe('RomanNumeralsConverter Class', () => {
     });
   });
   describe('#translateIntoModern', () => {
-    it('returns a 1 for I', () => {
-      expect(roman.translateIntoModern("I")).toBe(1);
-    });
-    it('returns a 5 for V', () => {
-      expect(roman.translateIntoModern("V")).toBe(5);
-    });
-    it('returns a 10 for X', () => {
-      expect(roman.translateIntoModern("X")).toBe(10);
-    });
-    it('returns a 50 for L', () => {
-      expect(roman.translateIntoModern("L")).toBe(50);
-    });
-    it('returns a 100 for C', () => {
-      expect(roman.translateIntoModern("C")).toBe(100);
-    });
-    it('returns a 500 for D', () => {
-      expect(roman.translateIntoModern("D")).toBe(500);
-    });
-    it('returns a 1000 for M', () => {
-      expect(roman.translateIntoModern("M")).toBe(1000);
+    describe('for single letters', () => {
+      it('returns a 1 for I', () => {
+        expect(roman.translateIntoModern("I")).toBe(1);
+      });
+      it('returns a 5 for V', () => {
+        expect(roman.translateIntoModern("V")).toBe(5);
+      });
+      it('returns a 10 for X', () => {
+        expect(roman.translateIntoModern("X")).toBe(10);
+      });
+      it('returns a 50 for L', () => {
+        expect(roman.translateIntoModern("L")).toBe(50);
+      });
+      it('returns a 100 for C', () => {
+        expect(roman.translateIntoModern("C")).toBe(100);
+      });
+      it('returns a 500 for D', () => {
+        expect(roman.translateIntoModern("D")).toBe(500);
+      });
+      it('returns a 1000 for M', () => {
+        expect(roman.translateIntoModern("M")).toBe(1000);
+      });
     });
   });
 });
