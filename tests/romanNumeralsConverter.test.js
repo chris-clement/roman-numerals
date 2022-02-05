e2d234ea313784e51978931d0733432fddada0b6
Chris Clement <chrisclement@Chriss-Air.connect>
2022-02-05 12:10:17 +0000

    Refactored tests so new instance of RomanNumeralsConverter class is made using a beforeEach function

--- tests/romanNumeralsConverter.test.js
+++ tests/romanNumeralsConverter.test.js
@@ -2,12 +2,13 @@ const RomanNumeralsConverter = require('../src/romanNumeralsConverter.js')
 
 describe('RomanNumeralsConverter Class', () => {
   describe('#translateIntoRoman', () => {
+    beforeEach(() => {
+      roman = new RomanNumeralsConverter;
+    });
     it('returns a I for 1', () => {
-      const roman = new RomanNumeralsConverter;
       expect(roman.translateIntoRoman(1)).toBe("I");
     });
     it('returns a V for 5', () => {
-      const roman = new RomanNumeralsConverter;
       expect(roman.translateIntoRoman(5)).toBe("V");
     });
   });
