4a5021290f8f6d0899ee8dba977f674e89476f60
Chris Clement <chrisclement@Chriss-Air.connect>
2022-02-06 14:39:22 +0000

    removed weaker first attempt at a translateIntoModern method and associated tests

--- src/romanNumeralsConverter.js
+++ src/romanNumeralsConverter.js
@@ -53,61 +53,9 @@ class RomanNumeralsConverter{
     };
     return result;
   };
+
   findSpecialCase(givenRomanLetters, index) {
     return this.specialCase.find(element => element.numeral == givenRomanLetters[index] + givenRomanLetters[index + 1])
-  };
-  
-  translateIntoModern(romanLetters) {
-    if(this.checkForIX(romanLetters)) {
-      var romanLetters = romanLetters.replace("IX", "");
-      this.score += 9;
-    } else if(this.checkForIV(romanLetters)) {
-      var romanLetters = romanLetters.replace("IV", "");
-      this.score += 4;
-    };
-    if(this.checkForCM(romanLetters)) {
-      romanLetters = romanLetters.replace("CM", "");
-      this.score += 900;
-    };
-    if(this.checkForXC(romanLetters)) {
-      romanLetters = romanLetters.replace("XC", "");
-      this.score += 90;
-    };
-    var romanLettersArray = romanLetters.split('')
-    romanLettersArray.forEach((romanLetter) => {
-      this.translateRomanLetterIntoModern(romanLetter);
-    });
-    return this.score;
-  };
-  translateRomanLetterIntoModern(single_letter) {
-    if(single_letter == "I") {
-      this.score += 1;
-    } else if(single_letter == "V") {
-      this.score += 5;
-    } else if(single_letter == "X") {
-      this.score += 10; 
-    } else if(single_letter == "L") {
-      this.score += 50; 
-    } else if(single_letter == "C") {
-      this.score += 100;
-    } else if(single_letter == "D") {
-      this.score += 500;
-    } else if(single_letter == "M") {
-      this.score += 1000;
-    };
-    return this.score;
-  };
-  checkForIX(romanLetters) {
-    return romanLetters.slice(-2) == "IX";
-  };
-  checkForIV(romanLetters) {
-    return romanLetters.slice(-2) == "IV";
-  };
-  checkForCM(romanLetters) {
-    return romanLetters.includes("CM");
-  };
-  checkForXC(romanLetters) {
-    return romanLetters.includes("XC");
   };
 };
 
